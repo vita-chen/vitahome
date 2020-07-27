ca4b83ca350763ba97e67ed57e36f4912fa07c81
Vita-Chen <vir829719@gmail.com>
2020-07-27 13:37:42 +0800

    init commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Today is " + args[0]+ " " +args[1]);
+    }
+}
+
+//C:\EA102_Workspace
+//javac HelloWorld.java
+//java HelloWorld Monday "Today is nice!"
