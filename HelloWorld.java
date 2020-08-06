957ae93d2586e1c88c348136096ee6c1a9bb51fb
Vita-Chen <vir829719@gmail.com>
2020-08-06 14:49:29 +0800

    revert & reset

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("thueuyh Hello!!!! 12155 00");
+    }
+}
+
+//C:\EA102_Workspace
+//javac HelloWorld.java
+//java HelloWorld Monday "Today is nice!"
--- World.java
+++ World.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("thueuyh Hello");
+        System.out.println("thueuyh Hello!!!! 12155 00");
+        System.out.println("4548ejrije");
     }
 }
 
