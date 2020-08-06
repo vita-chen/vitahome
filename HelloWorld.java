786e8200a6b6c61d9a7cb6771ff4f259a7d4ba30
Vita-Chen <vir829719@gmail.com>
2020-08-06 16:29:41 +0800

    localhost

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello!~");
-        System.out.println("World~~~~~~~");
+        System.out.println("World~~~~~~~111");
 
     }
 }
